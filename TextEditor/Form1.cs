00efd8dd519e25747ddb832e8dfe42840378c20f
Yan <none@none.com>
2023-10-30 15:05:38 +0200

    Чистый проект

--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -5,7 +5,6 @@ namespace TextEditor
         public Main_Form()
         {
             InitializeComponent();
-          //Получение из origin
         }
 
         private void Main_Form_FormClosing(object sender, FormClosingEventArgs e)
